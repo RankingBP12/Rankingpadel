fbc8be96164a2ec58af865674547a70e05a777d9
PochiTama-dev <gonzaloenrique@pochitama.dev>
2024-12-03 19:39:08 -0300

    analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactGA from 'react-ga4'; 
 
-import './index.css'
-import Router from './router/router.jsx'
+import './index.css';
+import Router from './router/router.jsx';
+
+
+ReactGA.initialize('G-7J7GSZ5GPR');  
+ReactGA.send('pageview');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router />
-  </StrictMode>,
-)
+  </StrictMode>
+);
